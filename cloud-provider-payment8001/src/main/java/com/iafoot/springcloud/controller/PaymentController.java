4fb0b046878b40f0ed3ec21a719bc92c7704d79b
hanrx <1170807870@qq.com>
2021-03-01 20:14:55 +0800

    添加 EurekaMain7001 模块。 并将 PaymentMain8001 注册进去 OrderMain80 注册进去 Eureka 添加 EurekaMain7001 模块。 添加 PaymentMain8002 注册进去 使服务具备基本负载均衡能力 --new PaymentMain8001 添加 服务发现Discovery

--- cloud-provider-payment8001/src/main/java/com/iafoot/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/iafoot/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.iafoot.springcloud.entities.Payment;
 import com.iafoot.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ：iAfoot
@@ -22,6 +25,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource//服务发现
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -45,7 +51,19 @@ public class PaymentController {
         }
     }
 
-
+    //服务发现
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element: services) {
+            log.info("******element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PROVIDER-SERVICE");
+        for (ServiceInstance instance: instances) {
+            log.info(instance.getInstanceId()+"\t"+instance.getHost()+"/t"+instance.getPort()+"/t"+instance.getUri());
+        }
+        return discoveryClient;
+    }
 
 
 

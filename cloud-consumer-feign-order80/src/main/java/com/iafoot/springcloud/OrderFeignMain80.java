e6fd67c478cf51193f16c7da2e964b20fb4fdf0a
hanrx <1170807870@qq.com>
2021-03-02 21:47:39 +0800

    添加 EurekaMain7001 模块。 并将 PaymentMain8001 注册进去 OrderMain80 注册进去 Eureka 添加 EurekaMain7001 模块。 添加 PaymentMain8002 注册进去 使服务具备基本负载均衡能力 PaymentMain8001 添加 服务发现Discovery --new 添加OpenFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/iafoot/springcloud/OrderFeignMain80.java
@@ -0,0 +1,18 @@
+package com.iafoot.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author ：iAfoot
+ * @description：TODO
+ * @date ：2021/3/2 21:25
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}

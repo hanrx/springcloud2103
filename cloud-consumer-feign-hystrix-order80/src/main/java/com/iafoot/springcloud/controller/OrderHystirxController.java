c14d873b92918f57bcf4b42fef1ea5ce369114c4
hanrx <1170807870@qq.com>
2021-03-03 21:06:32 +0800

    添加 EurekaMain7001 模块。 并将 PaymentMain8001 注册进去 OrderMain80 注册进去 Eureka 添加 EurekaMain7001 模块。 添加 PaymentMain8002 注册进去 使服务具备基本负载均衡能力 PaymentMain8001 添加 服务发现Discovery 添加OpenFeign OpenFeign超时控制 OpenFeign日志打印功能 hystrix 基础环境 --new  添加 添加客户端 服务端，方法级别服务降级实现

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/iafoot/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/iafoot/springcloud/controller/OrderHystirxController.java
@@ -1,12 +1,15 @@
 package com.iafoot.springcloud.controller;
 
 import com.iafoot.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ：iAfoot
@@ -31,13 +34,26 @@ public class OrderHystirxController {
      * @return
      */
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
     }
 
 
-
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        int timeNumber = 5;
+        try {
+            // 暂停5秒钟
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:" + Thread.currentThread().getName() + " 我是消费者，服务超时了，id:" + id + "\t" +
+                "ERROR" ;
+    }
 
 
 
